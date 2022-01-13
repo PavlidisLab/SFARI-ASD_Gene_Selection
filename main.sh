dc5720d84d1b05e1212314c8dcb7ce8d20eced8a
k8iechen <k8iechen@gmail.com>
2022-01-13 11:20:24 -0800

    pipeline set-up

---
+++ main.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# This is the main program for the SFARI ASD Gene Selection/Prioritization porject, 
+# Aim 1.1 from the Proposal Narrative.
+
+# Script Info====================================
+# ===============================================
+readonly PROGNAME=$(basename ${BASH_SOURCE[0]});
+readonly PROGDIR=$(readlink -m $(dirname ${BASH_SOURCE[0]}));
+
+# Input Params===================================
+# ===============================================
+if [[ $# -lt 5 ]]; then
+        #echo "Usage: $PROGNAME GENE_LIST DB1 DB2 [...]".
+	echo "Usage: $PROGNAME GENE_LIST <DIOPT> <GENE2PUBMED> <CLINVAR> <VARICARTA>"
+	exit;
+fi
+
+readonly GENE_LIST="$1"; shift;
+#readonly -a DBS=("$@");
+readonly DIOPT="$1"; shift;
+readonly GENE2PUBMED="$1"; shift;
+readonly CLINVAR="$1"; shift;
+readonly VARICARTA="$1"; shift;
+
+# Main Pipeline==================================
+# ===============================================
+main () {
+
+
+} #main
+
+
