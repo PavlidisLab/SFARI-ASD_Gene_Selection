a4693fe64631013ee9de522fbe296c502d1c035b
k8iechen <k8iechen@gmail.com>
2022-01-19 16:33:50 -0800

    minor fix

--- utils/get_main_csv.py
+++ utils/get_main_csv.py
@@ -5,12 +5,13 @@ import itertools
 import os
 
 OUTDIR=sys.argv[1]
-PKL=sys.argv[2] #pickle file
+#PKL=sys.argv[2] #pickle file
 
-with open(PKL, 'rb') as f, open(os.path.join(OUTDIR,"main.csv"),'w') as g:
+with open(os.path.join(OUTDIR,'dict_main.pickle'), 'rb') as f, open(os.path.join(OUTDIR,"main.csv"),'w') as g:
 	d = pickle.load(f)
 	#print(d)
 	headers = ['gene', 'Source', 'SFARI_score'] #list of column fields wanted
+
 
 	w = csv.DictWriter( g, fieldnames=headers)
 	w.writeheader()
